9f64e4b37e90eb8ce57fd1070b0668a7f64be70a
shivaananthagiri <shiva.ananthagiri2507@gmail.com>
2024-07-17 13:41:32 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "ami_id" {
+  type = string
+  default = "ami-0b4f379183e5706b9"
+}
+
+variable "instance_names" {
+  type = map
+    default = {
+    Name = "mongodb"
+  }
+}
+
+# variable "instance_type" {
+#   type = string
+#   default = ""
+# }
+
